497b6896338b5ce6e7e7e16e3ebfe55a91d8f875
dharmi027 <dharmivaghasiya603@gmail.com>
2026-02-08 14:45:11 +0530

    Initial commit - Project Management System

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# LOGIN PAGE (First open)
 @app.route('/')
 def login():
     return render_template("login.html")
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template("dashboard.html")
 
+# After login → Dashboard
+@app.route('/dashboard', methods=['POST'])
+def dashboard():
+    username = request.form.get("username")
+    return render_template("dashboard.html", username=username)
+
+
+# Create Project Page
 @app.route('/create_project')
 def create_project():
     return render_template("create_project.html")
+
+
+# Project Members Page
+@app.route('/project_members', methods=['POST'])
+def project_members():
+    project_name = request.form.get("project_name")
+    print("DEBUG:", project_name) 
+    return render_template("project_members.html", project_name=project_name)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
